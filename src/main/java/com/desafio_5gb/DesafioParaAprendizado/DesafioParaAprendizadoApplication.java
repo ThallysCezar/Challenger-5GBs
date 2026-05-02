65a8edee2529afdfbb7bf8d26fb678aa94149858
Thallys Cezar | Grupo Moura <thallys.cezar@grupomoura.com>
2026-05-02 15:58:50 -0300

    :fix: Adicionando correções no código e adicionando a porta correta no docker-compose

--- src/main/java/com/desafio_5gb/DesafioParaAprendizado/DesafioParaAprendizadoApplication.java
+++ src/main/java/com/desafio_5gb/DesafioParaAprendizado/DesafioParaAprendizadoApplication.java
@@ -1,4 +1,4 @@
-package com.desafio_5gb.Desafio.para.aprendizado;
+package com.desafio_5gb.DesafioParaAprendizado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
